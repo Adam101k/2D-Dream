7c7673880ed9e1800e3883890ecf88b5bc00147e
Stickguy1 <adamkaci3@gmail.com>
2022-09-03 17:45:44 -0700

    Wip movement

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-
+    float moveHorizontal;
+    float moveVertical;
     public float speed = 10f;
+    public float jumpPower = 2f;
     
     private Rigidbody2D rb;
     // Start is called before the first frame update
@@ -15,12 +17,17 @@ public class movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb.AddForce(movement * speed);
+        moveHorizontal = Input.GetAxis("Horizontal");
+        //moveVertical = Input.GetAxis("Vertical");
+        Jump();
+        rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
+    }
+
+    private void Jump() {
+        if(Input.GetButtonDown("Jump")) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
     }
 }
