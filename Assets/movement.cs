9c7a9b2233156c3796d01c864542d516f4263713
Stickguy1 <adamkaci3@csu.fullerton.edu>
2022-09-11 22:46:37 -0700

    Movement Adjustment + Broken Idle Animation

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -6,28 +6,22 @@ public class movement : MonoBehaviour
 {
     float moveHorizontal;
     float moveVertical;
+    private Rigidbody2D rb;
+    Vector2 move;
     public float speed = 10f;
     public float jumpPower = 2f;
-    
-    private Rigidbody2D rb;
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        moveHorizontal = Input.GetAxis("Horizontal");
-        //moveVertical = Input.GetAxis("Vertical");
-        Jump();
-        rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
+        rb.velocity = new Vector2(move.x * speed, rb.velocity.y);
+    }
+    void Update()
+    {
+        move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 
-    private void Jump() {
-        if(Input.GetButtonDown("Jump")) {
-            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
-        }
-    }
 }
